6872c64d3c4bde4a026bd23c5994ee9190928a08
GlobalEye Admin <admin@globaleye.live>
2025-07-19 17:47:23 +0300

    معالجة وعود غير معالجة وإزالة non-null assertion حسب توصيات DeepSource

--- src/components/ArticleHeader.tsx
+++ src/components/ArticleHeader.tsx
@@ -83,7 +83,7 @@ export default function ArticleHeader({ article }: { article: NewsArticle }) {
           }
         }
       };
-      checkFavorite();
+      void checkFavorite();
     }
   }, [user, article.slug]);
 
