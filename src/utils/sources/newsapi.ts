43636c23224948f5da8b219baa5b4a48cb6a97fa
Ahmed Eid <ahmed3id333@gmail.com>
2025-07-21 23:50:33 +0300

    Update newsapi.ts

--- src/utils/sources/newsapi.ts
+++ src/utils/sources/newsapi.ts
@@ -1,16 +1,17 @@
-export async function getNewsFromNewsApi(category: string) {
+export async function getNewsFromNewsAPI(category: string) {
   const apiKey = process.env.NEWSAPI_KEY;
-  const url = `https://newsapi.org/v2/top-headlines?category=${category}&apiKey=${apiKey}&language=en`;
+  const response = await fetch(`https://newsapi.org/v2/top-headlines?category=${category}&language=en&apiKey=${apiKey}`);
 
-  const res = await fetch(url);
-  const data = await res.json();
+  if (!response.ok) throw new Error('Failed to fetch from NewsAPI');
+
+  const data = await response.json();
 
   return data.articles.map((article: any) => ({
-    source: 'NewsAPI',
     title: article.title,
     description: article.description,
     url: article.url,
     image: article.urlToImage,
     publishedAt: article.publishedAt,
+    source: 'NewsAPI',
   }));
 }
