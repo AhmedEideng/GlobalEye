4a6f008d322181d33ec67ef159e423d74b6b92a8
Ahmed Eid <ahmed3id333@gmail.com>
2025-07-22 00:30:24 +0300

    Update gnews.ts

--- src/utils/sources/gnews.ts
+++ src/utils/sources/gnews.ts
@@ -1,21 +1,12 @@
-// تعريف نوع Article
-type GNewsArticle = {
-  title: string;
-  description: string;
-  url: string;
-  image: string;
-  publishedAt: string;
-};
-
 export async function getNewsFromGNews(category: string) {
   const apiKey = process.env.GNEWS_KEY;
   const response = await fetch(`https://gnews.io/api/v4/top-headlines?topic=${category}&lang=en&token=${apiKey}`);
 
   if (!response.ok) throw new Error('Failed to fetch from GNews');
 
-  const data = await response.json();
+  const data: { articles: GNewsArticle[] } = await response.json();
 
-  return data.articles.map((article: GNewsArticle) => ({
+  return data.articles.map((article) => ({
     title: article.title,
     description: article.description,
     url: article.url,
@@ -23,4 +14,12 @@ export async function getNewsFromGNews(category: string) {
     publishedAt: article.publishedAt,
     source: 'GNews',
   }));
+}
+
+interface GNewsArticle {
+  title: string;
+  description: string;
+  url: string;
+  image: string;
+  publishedAt: string;
 }
