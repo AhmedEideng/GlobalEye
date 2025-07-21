72954b885051fdf3acfd76a73775cbf2f1e13e43
Ahmed Eid <ahmed3id333@gmail.com>
2025-07-21 23:49:43 +0300

    Update gnews.ts

--- src/utils/sources/gnews.ts
+++ src/utils/sources/gnews.ts
@@ -1,16 +1,17 @@
 export async function getNewsFromGNews(category: string) {
   const apiKey = process.env.GNEWS_KEY;
-  const url = `https://gnews.io/api/v4/top-headlines?topic=${category}&token=${apiKey}&lang=en`;
+  const response = await fetch(`https://gnews.io/api/v4/top-headlines?topic=${category}&lang=en&token=${apiKey}`);
 
-  const res = await fetch(url);
-  const data = await res.json();
+  if (!response.ok) throw new Error('Failed to fetch from GNews');
+
+  const data = await response.json();
 
   return data.articles.map((article: any) => ({
-    source: 'GNews',
     title: article.title,
     description: article.description,
     url: article.url,
     image: article.image,
     publishedAt: article.publishedAt,
+    source: 'GNews',
   }));
 }
