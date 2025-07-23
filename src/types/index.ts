af3e4f8a218c89debfa2f8c2ed78b1ee165f435c
Ahmed Eid <ahmed3id333@gmail.com>
2025-07-23 20:22:52 +0300

    Create index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export interface NewsItem {
+  title: string | null;
+  description: string | null;
+  content: string | null;
+  url: string;
+  image_url: string | null;
+  published_at: string | null;
+  source: string;
+  author: string | null;
+}
